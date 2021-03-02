7bf161d6ba0d1487c3fb11594c878f49d2d17abd
luisniola <luisniola09@gmail.com>
2021-03-02 22:05:43 +0000

    Was getting an error for the bool operator. Then I figured out that I was missing the constant after the parenthesis in line 25.

--- main.cpp
+++ main.cpp
@@ -5,27 +5,49 @@
 //------------------------------
 
 // Put the assignment code here
+#include <iostream>
+
 
 class Quaternion {
-  public:
+  
   double a;
   double b;
   double c;
   double d;
-
-  bool operator==(const Quaternion p){}
-
-  Quaternion(double p, double q, double r, double s)
+  public:
+  Quaternion(double e, double f, double g, double h )
   {
-    a=p;
-    b=q;
-    c=r;
-    d=s;
+    a=e;
+    b=f;
+    c=g;
+    d=h;
+  }
+   bool operator== (const Quaternion &e)const
+   {
+    if ((e.a == this-> a) && (e.b == this-> b) && (e.c == this-> c) && (e.d == this-> d))
+    return true;
+
+    else
+    return false;
+
   }
 
-  Quaternion operation+(const Quaternion p)
-  {
+  Quaternion operator+(const Quaternion &e){
+    Quaternion result(a+e.a,b+e.b,c+e.c,d+e.d);
+    return result;
+  
 
+  }
+
+  Quaternion operator-(const Quaternion &e){
+    Quaternion result(a-e.a, b-e.b, c-e.c, d-e.d);
+    return result;
+    
+  }
+  Quaternion operator*(double h){
+    Quaternion result(a*h,b*h,c*h,d*h);
+    return result;
+  
   }
  
    
